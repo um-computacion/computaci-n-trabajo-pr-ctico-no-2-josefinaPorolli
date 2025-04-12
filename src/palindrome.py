37a385bf7e7bee796f6f2686017cf33626b2a011
josefinaPorolli <j.porolli@alumno.um.edu.ar>
2025-04-12 20:57:26 -0300

    Fase 2: Implementación de la función - limpieza de texto

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,3 +1,13 @@
 def is_palindrome(word):
+
+    #Limpieza de caracteres especiales, espacios, mayúsculas y minúsculas
+    # Caracteres especiales y espacios
+    special_chars = [' ', '.', ',', '!', '?', ';', ':', '-', '_', '(', ')', '[', ']', '{', '}', "'", '"']
+    word = word.lower() # Para no diferenciar entre mayúsculas y minúsculas
+    
+    # Eliminar caracteres especiales y espacios
+    for char in special_chars:
+        word = word.replace(char, '')
+
     return word == word[::-1]
     
